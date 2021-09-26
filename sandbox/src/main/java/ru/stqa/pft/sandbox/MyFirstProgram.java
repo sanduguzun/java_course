13fb7c8e820ead5d9abbf6173db2610af3cec940
Sandu Guzun <64661429+sanduguzun@users.noreply.github.com>
2021-09-26 11:42:35 +0200

    Project disconnected from Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-
-    System.out.println("Hello World");
+      System.out.println("Hello World");
   }
 
 }
