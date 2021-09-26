aa23f238c2ac7c271c03f8c86dc8810c067f1aff
Sandu Guzun <64661429+sanduguzun@users.noreply.github.com>
2021-09-26 20:07:10 +0200

    New func added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-      System.out.println("Hello World");
+      hello("world");
+      hello("user");
+      hello("Sandu");
+
+      double l = 5;
+      System.out.println("area of square with sides " + l + " = " + area(l));
+
+      double a = 4;
+      double b = 6;
+      System.out.println("area of square with sides " + a + " and " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+      return l * l;
+
+  }
+
+  public static double area(double  a, double  b) {
+      return a * b;
+
   }
 
 }
