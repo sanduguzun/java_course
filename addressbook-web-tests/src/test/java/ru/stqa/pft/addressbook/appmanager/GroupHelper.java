3a98053877a1324037831a22984a6e75ff678f61
Sandu Guzun <64661429+sanduguzun@users.noreply.github.com>
2021-10-23 19:57:03 +0200

    Задание №6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,4 +35,12 @@ public class GroupHelper extends HelperBase {
   public void selectGroup() {
     click(By.xpath("//div[@id='content']/form/span[3]/input"));
   }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModofication() {
+    click(By.name("update"));
+  }
 }
