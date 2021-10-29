b7807c65ff72eca686ce4f6e08acc32266f9cea7
Sandu Guzun <64661429+sanduguzun@users.noreply.github.com>
2021-10-29 21:41:32 +0200

    Лекция 3.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -16,5 +16,4 @@ public class GroupCreationTests extends TestBase {
     app.logOutAddressBook();
   }
 
-
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,20 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.AplicationManager;
 
 public class TestBase {
 
-  protected final AplicationManager app = new AplicationManager();
+  protected final AplicationManager app = new AplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeMethod
   public void setUp() throws Exception {
     app.init();
   }
 
 
-  @AfterMethod(alwaysRun = true)
+  @AfterMethod
   public void tearDown() throws Exception {
     app.stop();
   }
